dd0f18aaf361c4e8a7170dd1dfecb7b87c700447
grillazz <the@grillazz.com>
2016-08-16 16:38:26 +0200

    add CassandraAdventuresSerializer

--- drf_cassandra_boilerplate/api_example/serializers.py
+++ drf_cassandra_boilerplate/api_example/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework.serializers import Serializer
-from rest_framework.fields import URLField, CharField, DateTimeField
+from rest_framework.fields import UUIDField, CharField, DateTimeField
 
 
 class CassandraAdventureSerializer(Serializer):
-    adventure_id = URLField(format='hex_verbose')
+    adventure_id = UUIDField(format='hex_verbose')
     adventure_desc = CharField()
     last_modified = DateTimeField()
